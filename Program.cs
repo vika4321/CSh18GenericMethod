b8f6b74e1afc34a037c2696bf9ac17b851ea2bee
Victoria Stampfli <vika4321@yahoo.com>
2020-01-06 11:19:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryTree;
+
+namespace BuildTree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Tree<char> charTree = new Tree<char>('M');
+			InsertIntoTree<char>(charTree, 'X', 'A', 'M', 'Z', 'Z', 'N');
+			charTree.WalkTree();
+			
+		}
+		static void InsertIntoTree<TItem>(Tree<TItem> tree, params TItem[] data) 
+			where TItem : IComparable<TItem>
+		{
+			if (data.Length == 0)
+				throw new ArgumentException("Must provide at least one data value");
+
+			foreach (TItem datum in data)
+			{
+				tree.Insert(datum);
+			}
+		}
+	}
+}
